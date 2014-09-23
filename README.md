96deafbf877c8c9bf0e6a4e1fef2739fafc8cea9
Cristiano Amici <amicicristiano@gmail.com>
2014-09-22 22:16:06 -0500

    Add script to download modules, install instructions in README
    Since the modues weren't (rightfully so) pushed to the repository, I created a shell that clones all included modules and copies them to the
    bundle directory. The shell should be maintained and modified as more modules are included. Also, I added an Installation section in the README.

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ This is just my VIM configuration. It will probably be a work in progress, as I 
 Although I use other editors in graphical mode, I still like to have a decent VIM configuration whenever I have to quickly check/edit some code through terminal (through SSH, for example).
 
 Feel free to check the .vimrc and/or my used plugins, I'll try to keep it as well-documented as possible. Any suggestion is appreciated!
+
+
+Installation
+------------
+
+1. Copy the `.vimrc` config file and the `.vim` directory on your home:
+```sh
+git clone https://github.com/AChris07/vim-config.git
+cp vim-config/.vimrc ~/
+cp vim-config/.vim ~/
+```
+
+2. Run the script to download the latest version of the modules used:
+```sh
+./vim-config/modules.sh
+```
---
+++ modules.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if command -v foo >/dev/null 2>&1; then
+  echo "Git is required to download the VIM modules included with this config."; exit 1;
+else
+  if [ ! -d "$HOME/.vim" ]; then
+    echo "Make sure to copy .vim in your home directory!"; exit 1;
+  else
+    if [ ! -d "$HOME/.vim/bundle" ]; then
+      mkdir "$HOME/.vim/bundle"; exit 1;
+    fi
+
+    # Solarized colorscheme for Vim
+    git clone https://github.com/altercation/vim-colors-solarized.git "$HOME/.vim/bundle/vim-colors-solarized"
+
+    # Gundo, visualizes your Vim undo tree
+    git clone https://github.com/sjl/gundo.vim.git "$HOME/.vim/bundle/gundo"
+
+    # CtrlP, module for fuzzy search
+    git clone https://github.com/kien/ctrlp.vim.git "$HOME/.vim/bundle/ctrlp.vim"
+
+    echo "Modules downloaded and configured correctly"
+    exit 0;
+  fi
+fi 
